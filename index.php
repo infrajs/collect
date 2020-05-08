438ef37e45283ebf8dbf01468b849fddc9266b53
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-08 12:16:49 +0400

    fix

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace infrajs\collect;
+
 use infrajs\access\Access;
 use infrajs\load\Load;
 use infrajs\mem\Mem;
@@ -9,14 +11,62 @@ use MatthiasMullie\Minify;
 use infrajs\router\Router;
 use infrajs\config\Config;
 use akiyatkin\fs\FS;
+use infrajs\rest\Rest;
 use infrajs\path\Path;
 use infrajs\config\search\Search;
 
-$time = Ans::GET('time','string', ''); 
+Nostore::pub();
+
+$code = Rest::get(function () {
+	echo '/js или /css';
+}, 'js', function () {
+	$srcs = [];
+	Config::run( function ($name, $conf) use (&$srcs){
+		if (empty($conf['js'])) return;
+		$jss = $conf['js'];
+		if (!is_array($jss)) $jss = [$jss];
+		foreach ($jss as $js) {
+			$src = Path::theme('-'.$name.'/'.$js);
+			if (Path::getExt($src) == 'php') $src = '-'.$name.'/'.$js;
+			$srcs[] = $src;
+		}
+	});
+	header("Content-Type: application/javascript");
+	$html = '';
+	$i = 0;
+	foreach ($srcs as $src) {
+		$i++;
+		$html .= "import t".$i." from '/".$src."'\n";
+	}
+	return $html;
+}, 'css', function () {
+	$srcs = [];
+	Config::run( function ($name, $conf) use (&$srcs){
+		if (empty($conf['css'])) return;
+		$jss = $conf['css'];
+		if (!is_array($jss)) $jss = [$jss];
+		foreach ($jss as $js) {
+			$src = Path::theme('-'.$name.'/'.$js);
+			if (Path::getExt($src) == 'php') $src = '-'.$name.'/'.$js;
+			$srcs[] = $src;
+		}
+	});
+	header("Content-Type: text/css");
+	$html = '';
+	foreach ($srcs as $src) {
+		$html .= "\n\n".'/*load css '.$src."*/\r\n";
+		$html .= Load::loadTEXT('-csspath/?src='.$src);
+	}
+	return $html;
+});
+echo $code;
+return;
+
+$time = Ans::GET('time', 'string', '');
 if ($time) Nostore::pubStat(); //Кэшируется, если public разрешён, как статика, надолго. Если указана версия
 
-if(isset($_GET['js'])) $isjs = 'js';
-else if(isset($_GET['css'])) $isjs = '';
+if (isset($_GET['js'])) $isjs = 'js';
+else if (isset($_GET['css'])) $isjs = '';
 else die('Необходимо добавить параметр css или js');
 
 if (!Load::isphp()) {
@@ -25,19 +75,19 @@ if (!Load::isphp()) {
 
 $re = isset($_GET['re']); //Modified re нужно обновлять с ctrl+F5
 $debug = Access::debug();
-$name = Ans::GET('name','string','');
+$name = Ans::GET('name', 'string', '');
 
 if ($debug || $re) {
 	if (!Load::isphp()) header('Collect-Cache: false');
-	
+
 	if ($isjs) $code = Collect::js($name);
 	else $code = Collect::css($name);
 
-	$key = 'Collect::Collect::' . $isjs . true . $time;//Кэш с zip
+	$key = 'Collect::Collect::' . $isjs . true . $time; //Кэш с zip
 	Mem::delete($key);
-	$key = 'Collect::Collect::' . $isjs . false . $time;//Кэш без zip
+	$key = 'Collect::Collect::' . $isjs . false . $time; //Кэш без zip
 	Mem::delete($key);
-	
+
 	if ($isjs) return Ans::js($code);
 	return Ans::css($code);
 }
@@ -63,7 +113,7 @@ if (!$code) {
 
 	if ($isjs) $code = Collect::js($name);
 	else $code = Collect::css($name);
-	
+
 	/*if ($isjs) $min = new Minify\JS($code);
 	else $min = new Minify\CSS($code);
 
